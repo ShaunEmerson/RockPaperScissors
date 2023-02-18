0efaafa98e3a90e7576ccf1543fd034a0cfed6c1
Shaun Emerson <109479721+ShaunEmerson@users.noreply.github.com>
2023-02-18 11:08:03 -0700

    Add notes for second step and write pseudocode.

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// STEP 1
+
 // Your game is going to play against the computer, 
 // so begin with a function called getComputerChoice 
 // that will randomly return either ‘Rock’, ‘Paper’ 
@@ -17,3 +19,16 @@ function getComputerChoice(choices) {
 
 const choices = ["rock", "paper", "scissors"];
 
+// STEP 2
+
+// Write a function that plays a single round of Rock Paper Scissors. 
+// The function should take two parameters - the playerSelection and computerSelection
+// and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+// Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
+
+
+// Create a playerSelection variable that takes its value from an input by the player.
+// Create a function that takes the playerSelection variable and evaluates it against the getComputerChoice function output.
+// Use if else statements to evaluate the player's selection against the computer.
+// Based on the evaluation, show a "winner" or "loser" message.
+// use toLowerCase to turn make all inputs equal to the computer's selection.
