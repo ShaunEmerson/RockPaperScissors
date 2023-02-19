959c63cc01e745ee746bb9dc1b538c6d3fe3382d
Shaun Emerson <109479721+ShaunEmerson@users.noreply.github.com>
2023-02-18 22:01:07 -0700

    Save copy before starting from scratch after multiple attempts at debugging problem that caused every game to display the wrong message.

--- main.js
+++ main.js
@@ -13,11 +13,19 @@
 // Create an array of options containing the strings "rock", "paper", and "scissors".
 // Create a function that randomly pulls a selection from the array and returns the selection.
 
+// getComputerChoice function gets the computer's choice using
+//  an array of options containing the strings "rock", "paper", and "scissors".
+
+let rock = "Rock";
+let paper = "Paper";
+let scissors ="Scissors";
+
+const choices = [`${rock}`, `${paper}`, `${scissors}`];
+
 function getComputerChoice(choices) {
     return choices[Math.floor(Math.random() * choices.length)];
 }
-
-const choices = ["rock", "paper", "scissors"];
+console.log(getComputerChoice(choices))
 
 // STEP 2
 
@@ -27,8 +35,45 @@ const choices = ["rock", "paper", "scissors"];
 // Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
 
 
-// Create a playerSelection variable that takes its value from an input by the player.
-// Create a function that takes the playerSelection variable and evaluates it against the getComputerChoice function output.
-// Use if else statements to evaluate the player's selection against the computer.
-// Based on the evaluation, show a "winner" or "loser" message.
-// use toLowerCase to turn make all inputs equal to the computer's selection.
+// playerInput variable takes its value from an input by the player.
+// playerSelection function uses toLowerCase to make input variable's string equal to the computer's selection.
+// computerSelection variable takes its value from the getComputerChoice function output.
+// playGame function takes the playerSelection variable and evaluates it against 
+//  computerSelection using if else statements to evaluate the player's selection against the computer.
+// Based on the evaluation, shows a message declaring the player a "winner", "loser", or a "tie".
+
+let computerSelection = getComputerChoice(choices);
+
+function playerInput() {
+    return prompt(`Please enter "${rock}", "${paper}", or "${scissors}"`);
+}
+
+const firstLetter = playerInput.charAt(0);
+
+const remainingLetters = playerInput.slice(1);
+
+playerSelection = firstLetter + remainingLetters;
+
+function playGame() {
+    let result;
+    if (playerSelection == "rock" && computerSelection == "scissors") {
+        result = "You win! Rock breaks Scissors!";
+    } else if (computerSelection == "rock" && playerSelection == "scissors") {
+        result = "You lose! Rock breaks Scissors!";
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        result = "You win! Scissors cuts Paper!";
+    } else if (computerSelection == "scissors" && playerSelection == "paper") {
+        result = "You lose! Scissors cuts Paper!";
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        result = "You win! Paper covers Rock!";
+    } else if (computerSelection == "paper" && playerSelection == "rock") {
+        result = "You lose! Paper covers Rock!";
+    } else {
+        result = "It's a tie!";
+    }
+    return result;
+    console.log(result);
+}
+
+alert(playerSelection());
+
