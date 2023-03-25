ea6afb9feb6acbf84f11230846380b52b71cc16b
Shaun Emerson <109479721+ShaunEmerson@users.noreply.github.com>
2023-03-24 18:02:13 -0600

    Rewrite entire code to function better and look cleaner. Create game function to play five rounds of the came and add previous functions inside game function. Add validation on playerSelection. Add score tracking.

--- main.js
+++ main.js
@@ -1,50 +1,89 @@
-// STEP 1
 
-// Create a function that randomly selects "rock", "paper", or "scissors".
-// Create a function named "getComputerChoice()".
-// Create a variable named "choices" that contains an array of the choices.
-// Create another variable named "selection" that randomly chooses a value from the "choices array".
-// Test the function with console.log() to verify it works.
+// Function that plays multiple rounds of Rock Paper Scissors
+function game(){
 
-let choices = ["rock", "paper", "scissors"];
+// List of variables used at game() function level
+    const choices = ["rock", "paper", "scissors"];
+    let computerSelection;
+    let playerSelection;
+    let playerScore = 0;
+    let computerScore = 0;
+    let round = 1;
 
-function getComputerChoice() {
-    let selection = choices[Math.floor(Math.random() * choices.length)];
-    return selection;
-}
+// Function randomly selects "rock", "paper", or "scissors" for computer to play against player
+    function getComputerChoice() {
+        let selection = choices[Math.floor(Math.random() * choices.length)];
+        return selection;
+    }
 
-// STEP 2
+// Creates a basic visual aspect to follow in the console
+    console.log("The best of 5 rounds wins!");
+    console.log("");
 
+// Iterates through the rounds until the game has been played five times
+    for (i = 0; i < 5; i++){
 
-// playerSelection is entered into a prompt and reduced toLowerCase for comparison with computerSelection.
-// computerSelection is equal to the result of getComputerChoice function.
-// playRound function evaluates playerSelection against computerSelection using if else statements.
-// Based on the evaluation, shows an alert message declaring a "winner", "loser", or a "tie".
+// Function that plays a round of Rock Paper Scissors
+        function playRound(playerSelection, computerSelection) {
+            console.log("Round: " + round);
+            computerSelection = getComputerChoice();
+            playerSelection = prompt(`Please enter ${choices[0]}, ${choices[1]}, or ${choices[2]}.`, "rock").toLowerCase();
 
-let computerSelection = getComputerChoice();
+// Validates the player's choice to ensure it is one of the correct options
+            if (playerSelection === "rock" ||
+                playerSelection === "paper" ||
+                playerSelection === "scissors") {
+                console.log(`You chose ${playerSelection}, the computer chose ${computerSelection}.`);
+            } else {
+                playerSelection = prompt(`You made an invalid choice. Please enter ${choices[0]}, ${choices[1]}, or ${choices[2]}.`, "rock")
+            }
 
-let playerSelection = prompt(`Please enter ${choices[0]}, ${choices[1]}, or ${choices[2]}.`).toLowerCase();
+// Evaluates choices and displays a message declaring a winner, loser, or tie, and updates the scores and jumps to the next round
+            if (playerSelection === computerSelection) {
+            console.log(`It's a tie, you chose the same as the computer.`)
+            } else if (playerSelection === choices[0] && computerSelection === choices[1] ||
+                playerSelection === choices[1] && computerSelection === choices[2] || 
+                playerSelection === choices[2] && computerSelection === choices[0]) {
+                computerScore += 1;
+                console.log(`You lose, ${computerSelection} beats ${playerSelection}!`);
+            } else {
+                playerScore += 1;
+                console.log(`You win, ${playerSelection} beats ${computerSelection}!`);
+            }
+            round++;
+        }
 
-function playRound() {
-    if (playerSelection === computerSelection) {
-        let result = "it's a tie";
-        return result;
-    } else if (playerSelection === choices[0] && computerSelection === choices[1] || playerSelection === choices[1] && computerSelection === choices[2] || playerSelection === choices[2] && computerSelection === choices[0]) {
-        let result = `You lose, ${computerSelection} beats ${playerSelection}!`;
-        return result;
-    } else if (playerSelection === choices[1] && computerSelection === choices[0] || playerSelection === choices[2] && computerSelection === choices[1] || playerSelection === choices[0] && computerSelection === choices[2]) {
-        let result = `You win, ${playerSelection} beats ${computerSelection}!`;
-        return result;
+// Calls the round to begin
+        playRound();
+
+// Displays the score of the previous round
+        console.log(`Score          Player: ${playerScore}      Computer: ${computerScore}`);
+        console.log("")
+    }
+
+// Evaluates the player's score against the computer's score and declares the winner of the game
+    if (playerScore === computerScore) {
+        if (playerScore === 0 && computerScore === 0) {
+            console.log("It's a tie. You and the computer have tied every round.");
+        } else {
+            console.log("It's a tie. You've won the same amount of rounds as the computer.");
+        }
+    } else if (playerScore > computerScore) {
+        if (computerScore === 1){ 
+        console.log(`You win! You've won ${playerScore} rounds, while the computer has won only 1 round.`)
+        } else {
+        console.log(`You win! You've won ${playerScore} rounds, while the computer has won only ${computerScore} rounds.`)
+        }
+    } else {
+        if (playerScore === 1) {
+        console.log(`You lose! You've won only 1 round, while the computer has won ${computerScore} rounds.`)
+        } else if (playerScore === 0){
+            console.log(`You lose! You've won 0 rounds, while the computer has won ${computerScore} rounds.`)
+        } else {
+            console.log(`You lose! You've won only ${playerScore} rounds, while the computer has won ${computerScore} rounds.`)
+        }
     }
 }
 
-alert(playRound());
-
-// STEP 3
-
-// Create game() function that iterates matches with a loop until five matches are played.
-// Use console.log to display the results after each match and the winner of the five rounds.
-
-// Function somehow calls playRound() to run one game of Rock Paper Scissors.
-// Once round is completed, function displays the results and re-runs the playround() game.
-// As long as the current game is less than the 5th one, reitterates with a loop.
+// Calls the game to play
+game();
